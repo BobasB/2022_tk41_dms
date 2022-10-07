2a06534f0b13336f19148a490df1ce7ea805ff86
Bohdan <bugil.bogdan@gmail.com>
2022-10-07 09:31:42 +0300

    lab4 final

--- 4_lab/lab.py
+++ 4_lab/lab.py
@@ -5,23 +5,28 @@ import logging
 # Site-packages
 #import ipykernel
 #import redis
-import pandas as pd
+#import pandas as pd
 
 
-print(sys.path)
+#print(sys.path)
 #print(ipykernel.__doc__)
 
-df = pd.DataFrame(
-    {
-        "Name": [
-            "Braund, Mr. Owen Harris",
-            "Allen, Mr. William Henry",
-            "Bonnell, Miss. Elizabeth",
-        ],
-        "Age": [22, 35, 58],
-        "Sex": ["male", "male", "female"],
-    }
-)
+#df = pd.DataFrame(
+#    {
+#        "Name": [
+#            "Braund, Mr. Owen Harris",
+#            "Allen, Mr. William Henry",
+#            "Bonnell, Miss. Elizabeth",
+#        ],
+#        "Age": [22, 35, 58],
+#        "Sex": ["male", "male", "female"],
+#    }
+#)
 
 
-print(df)
+#print(df)
+#os.environ["MY_VAR_2"] = "My_new_var"
+#print(os.environ["MY_VAR_2"])
+#os.environ["MY_VAR"] = "I have changed var"
+
+print(f"Виводимо нашу змінну середовища: {os.environ['MY_VAR']}")
